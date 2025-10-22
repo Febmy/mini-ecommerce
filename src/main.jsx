883b2e6f5d791854ad3cc4dd9a559d10699c9bee
Febmy <febmys.b@gmail.com>
2025-10-22 18:16:08 +0700

    push new

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
-import "./index.css";
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
+import './index.css'
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter
       future={{
@@ -15,4 +15,4 @@ createRoot(document.getElementById("root")).render(
       <App />
     </BrowserRouter>
   </React.StrictMode>
-);
+)
